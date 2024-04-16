58331b6fc312869fe9c9b0a74f8a35c3e1cb5029
jimmy-martin <jimmy.martin952@gmail.com>
2024-04-16 23:42:29 +0200

    test(search): add minter and teabag unit test

--- __tests__/search/search.test.tsx
+++ __tests__/search/search.test.tsx
@@ -2,21 +2,60 @@ import mockedSearchResult from "./search-mock-response.json"
 import { render, waitFor } from "@testing-library/react"
 import NftCard from "@/components/custom/nfts/nft-card"
 import { searchSchema } from "@/validators/schemas/search/searchSchema"
+import { describe, expect, it } from "@jest/globals"
+import { NftSearchNftSchemaType } from "@/validators/schemas/search/nfts/nftSearchNftSchema"
+import { MinterSearchMinterSchemaType } from "@/validators/schemas/search/minters/minterSearchMinterSchema"
+import MinterCard from "@/components/custom/minters/minter-card"
+import { TeabagSearchTeabagSchemaType } from "@/validators/schemas/search/teabags/teabagSearchTeabagSchema"
+import TeaBagCard from "@/components/custom/teabags/teabag-card"
 
 describe("Search", () => {
   it("should render nft cards", async () => {
-    const nfts = searchSchema.parse(mockedSearchResult).nfts
+    const { nfts } = searchSchema.parse(mockedSearchResult)
 
-    for (const nft of nfts) {
-      const { getByText, getByAltText } = render(<NftCard {...nft} />)
-      expect(getByText(`@${nft.originalContent?.minter?.username}`)).toBeDefined()
-      expect(getByText(nft.description)).toBeDefined()
+    await Promise.all(nfts.map((nft) => testNftRender(nft)))
+  })
 
-      await waitFor(() => {
-        const image = getByAltText(`NFT ${nft.id.toString()}`) as HTMLImageElement
-        expect(image).toBeDefined()
-        expect.stringContaining(encodeURIComponent(nft.imageUrl))
-      })
-    }
+  it("should render minter cards", async () => {
+    const { minters } = searchSchema.parse(mockedSearchResult)
+
+    await Promise.all(minters.map((minter) => testMinterRender(minter)))
+  })
+
+  it("should render tea bag cards", () => {
+    const { teabags } = searchSchema.parse(mockedSearchResult)
+
+    teabags.forEach((teabag) => {
+      const { getByText } = render(<TeaBagCard {...teabag} />)
+
+      expect(getByText(`@${teabag.name}`)).toBeDefined()
+      expect(getByText(teabag.bio)).toBeDefined()
+    })
   })
 })
+
+async function testNftRender(nft: NftSearchNftSchemaType) {
+  const { getByText, getByAltText } = render(<NftCard {...nft} />)
+
+  expect(getByText(`@${nft.originalContent.minter.username}`)).toBeDefined()
+  expect(getByText(nft.description)).toBeDefined()
+
+  await waitFor(() => {
+    const image = getByAltText(`NFT ${nft.id.toString()}`) as HTMLImageElement
+    expect(image).toBeDefined()
+    expect(image.src).toContain(encodeURIComponent(nft.imageUrl))
+  })
+}
+
+async function testMinterRender(minter: MinterSearchMinterSchemaType) {
+  const { getByText, getByAltText } = render(<MinterCard {...minter} />)
+
+  expect(getByText(`@${minter.username}`)).toBeDefined()
+  expect(getByText(minter.bio)).toBeDefined()
+
+  await waitFor(() => {
+    const image = getByAltText(`Minter ${minter.id.toString()}`) as HTMLImageElement
+    expect(image).toBeDefined()
+    expect(image.src).toContain(encodeURIComponent(minter.avatarUrl))
+  })
+}
