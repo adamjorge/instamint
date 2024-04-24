68ce1db432d7264590304add957fb370da2cbea2
jimmy-martin <jimmy.martin952@gmail.com>
2024-04-24 20:48:02 +0200

    fix(prisma): add random createdAt value in comments

--- prisma/scripts/comment/create-comments.ts
+++ prisma/scripts/comment/create-comments.ts
@@ -2,5 +2,16 @@ import { PrismaClient } from "@prisma/client"
 import rawData from "./data.json"
 
 export function createComments(prisma: PrismaClient) {
-  return prisma.comment.createMany({ data: rawData })
+  const data = rawData.map((comment) => ({
+    content: comment.content,
+    minterId: comment.minterId,
+    nftId: comment.nftId,
+    createdAt: getRandomDate(new Date(2022, 0, 1), new Date())
+  }))
+
+  return prisma.comment.createMany({ data })
+}
+
+function getRandomDate(start: Date, end: Date) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
 }
