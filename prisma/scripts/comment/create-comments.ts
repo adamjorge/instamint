55594ec1e083db7cb5292b44c872fffa4826d658
jimmy-martin <jimmy.martin952@gmail.com>
2024-04-25 10:47:26 +0200

    feat(prisma, seed): add reply feature on fake data

--- prisma/scripts/comment/create-comments.ts
+++ prisma/scripts/comment/create-comments.ts
@@ -2,10 +2,12 @@ import { PrismaClient } from "@prisma/client"
 import rawData from "./data.json"
 
 export function createComments(prisma: PrismaClient) {
-  const data = rawData.map((comment) => ({
+  const data: Comment[] = rawData.map((comment: Comment) => ({
+    id: comment.id,
     content: comment.content,
     minterId: comment.minterId,
     nftId: comment.nftId,
+    parentId: comment.parentId,
     createdAt: getRandomDate(new Date(2022, 0, 1), new Date())
   }))
 
@@ -14,4 +16,12 @@ export function createComments(prisma: PrismaClient) {
 
 function getRandomDate(start: Date, end: Date) {
   return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+}
+
+interface Comment {
+  id: number
+  content: string
+  minterId: number
+  nftId: number
+  parentId: number | null
 }
