a5570bcc99a7de0053f8fea948ac0aeb19b44457
Adam <adam.mehdaouijorge@gmail.com>
2024-05-03 18:40:36 +0200

    chore: improved profile view

--- src/components/custom/profile/profile-changes.tsx
+++ src/components/custom/profile/profile-changes.tsx
@@ -1,6 +1,17 @@
 "use client"
 
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
 import { handleDeleteUser } from "@/lib/query/users/handleDeleteUser"
 import { useMutation } from "@tanstack/react-query"
 import { signOut } from "next-auth/react"
@@ -19,11 +30,32 @@ export default function ProfileChanges({ userId }: ChangesProps) {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center w-full">
-      <h2>Modifier le profil</h2>
-      <Button className="bg-red-500 text-white hover:bg-medium mt-5" onClick={handleClickOnDelete}>
-        Delete your account
-      </Button>
+    <div className="flex flex-col items-start ml-5 space-y-5">
+      <h2 className="font-bold text-xl">Modifier le profil</h2>
+      <Avatar>
+        <AvatarFallback>IN</AvatarFallback>
+      </Avatar>
+      <div>
+        <Label htmlFor="bio">Bio</Label>
+        <Textarea className="mt-3" placeholder="Bio is a work in progress feature" disabled />
+      </div>
+      <Dialog>
+        <DialogTrigger className="bg-red-500 text-white hover:bg-dark p-2 rounded-lg border">
+          Delete your account
+        </DialogTrigger>
+        <DialogContent className="rounded-lg">
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure you want to delete your account?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove
+              your data from our servers in the next wave.
+            </DialogDescription>
+          </DialogHeader>
+          <Button className="bg-red-500 text-white hover:bg-medium" onClick={handleClickOnDelete}>
+            I'm sure, delete my account
+          </Button>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
