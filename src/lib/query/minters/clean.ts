e971bddbda4a0e70c2fc20e5cbb856e47fa3b2c4
Adam <adam.mehdaouijorge@gmail.com>
2024-05-05 19:08:12 +0200

    chore: added 6 months rule for delete users

--- src/lib/query/minters/clean.ts
+++ src/lib/query/minters/clean.ts
@@ -2,13 +2,16 @@ import prisma from "@/lib/db"
 import { Minter, PrismaPromise, User } from "@prisma/client"
 
 export async function cleanMinters() {
+  const limitedDate = new Date()
+  limitedDate.setMonth(limitedDate.getMonth() - 6)
+
   const deletionUsersPromises: Array<PrismaPromise<User>> = []
   const deletionMintersPromises: Array<PrismaPromise<Minter>> = []
   const mintersToDelete = new Set<number>()
   const usersToDelete = await prisma.user.findMany({
     where: {
       deletedAt: {
-        not: null
+        lte: limitedDate
       }
     }
   })
