0e49f68b59646e88e281b950b591378ff0dfc46e
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2024-05-21 11:46:31 +0200

    refactor: reset password client component.

--- src/components/custom/reset-password/reset-password-page.tsx
+++ src/components/custom/reset-password/reset-password-page.tsx
@@ -11,22 +11,15 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { requestPasswordReset } from "@/lib/utils/reset-password/passwordResetService"
+import { formSchema } from "@/validators/schemas/reset-password/resetPasswordSchema"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useTranslations } from "next-intl"
 import { useForm } from "react-hook-form"
 import { toast } from "sonner"
-import { z } from "zod"
 
 export interface ResetFormData {
   email: string
 }
-
-export const formSchema = z.object({
-  email: z
-    .string()
-    .email({ message: "Invalid email address" })
-    .min(3, { message: "Email must be at least 3 characters" })
-})
 
 export default function ResetPasswordForm() {
   const t = useTranslations("resetPassword")
@@ -52,7 +45,7 @@ export default function ResetPasswordForm() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(handleSubmit)}
@@ -76,6 +69,6 @@ export default function ResetPasswordForm() {
           </Button>
         </form>
       </Form>
-    </main>
+    </div>
   )
 }
--- src/components/custom/reset-password/update-password/update-password-page.tsx
+++ src/components/custom/reset-password/update-password/update-password-page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import UpdatePasswordForm from "@/components/custom/reset-password/update-password/update-password-form"
 
 export default function UpdatePasswordPage() {
