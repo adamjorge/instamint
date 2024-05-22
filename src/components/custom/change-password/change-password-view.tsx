d25365ef6966fbc20ea588949e665445f514733a
Adam <adam.mehdaouijorge@gmail.com>
2024-05-22 16:09:42 +0200

    fix: resuable component after merge of [IN-37]

--- src/components/custom/change-password/change-password-view.tsx
+++ src/components/custom/change-password/change-password-view.tsx
@@ -1,14 +1,18 @@
 "use client"
 
 import ChangePasswordForm from "@/components/custom/change-password/change-password-form"
+import Spinner from "@/components/custom/spinner"
+import ErrorMessage from "@/components/ui/custom/error-message"
 import { confirmChangePassword } from "@/lib/query/users/confirmChangePassword"
 import findChangeToken from "@/lib/query/users/findChangeToken"
 import { useMutation, useQuery } from "@tanstack/react-query"
+import { useTranslations } from "next-intl"
 import { useRouter } from "next/navigation"
 import { toast } from "sonner"
 
 export default function ChangePasswordView(props: ChangePasswordViewProps) {
   const router = useRouter()
+  const t = useTranslations("global")
   const { token } = props
   const mutation = useMutation({
     mutationFn: ({
@@ -34,11 +38,11 @@ export default function ChangePasswordView(props: ChangePasswordViewProps) {
   })
 
   if (isPending) {
-    return <div className="text-center">Loading...</div>
+    return <Spinner />
   }
 
   if (error) {
-    return <div className="text-center">Error</div>
+    return <ErrorMessage message={t("error")} />
   }
 
   if (!data.tokenVerified) {
