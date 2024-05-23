fe3540073793b4e56a9eed57ff149af751f1d4b7
Adam Mehdaoui <90783059+adammehdaoui@users.noreply.github.com>
2024-05-23 09:27:40 +0200

    [IN-80] GPDR Statement with preview on profile page (#28)
    * chore: first version of gpdr statement
    
    * chore: privacy policy page
    
    * feat: translation for privacy policy
    
    * test: update e2e and unit

--- src/components/custom/profile/profile-menu/how-to-menu.tsx
+++ src/components/custom/profile/profile-menu/how-to-menu.tsx
@@ -1,20 +1,37 @@
 import { Button } from "@/components/ui/button"
+import LinkButton from "@/components/ui/custom/link-button"
 import { useTranslations } from "next-intl"
+import { useCallback } from "react"
 import { BsFillFileEarmarkPersonFill } from "react-icons/bs"
 import { MdNotificationsActive } from "react-icons/md"
+import { toast } from "sonner"
 
 export default function HowToMenu() {
   const t = useTranslations("howToMenu")
+  const handleClickOnWIP = useCallback(() => {
+    toast.error("This feature is still a work in progress.")
+  }, [])
 
   return (
     <div className="w-full flex flex-col items-center">
-      <p className="text-medium text-sm">{t("howTo")}</p>
+      <p className="text-medium text-sm" aria-label="how to use instamint">
+        {t("howTo")}
+      </p>
       <div className="mt-3 w-full flex flex-col items-start">
-        <Button className="bg-white text-black hover:bg-light w-full flex justify-start">
+        <LinkButton
+          href="/profile/changes"
+          withLocale
+          className="bg-white text-black hover:bg-light w-full flex justify-start"
+          aria-label="edit profile"
+        >
           <BsFillFileEarmarkPersonFill size={20} />
           <span className="ml-3">{t("changeProfile")}</span>
-        </Button>
-        <Button className="bg-white text-black hover:bg-light w-full flex justify-start">
+        </LinkButton>
+        <Button
+          className="bg-white text-black hover:bg-light w-full flex justify-start"
+          onClick={handleClickOnWIP}
+          aria-label="notifications"
+        >
           <MdNotificationsActive size={20} />
           <span className="ml-3">{t("notifications")}</span>
         </Button>
