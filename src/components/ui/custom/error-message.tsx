10fa819557d180d2d7692e4bdeb66d7e5d681b51
Adam <adam.mehdaouijorge@gmail.com>
2024-06-25 15:05:36 +0200

    feat: enhance UI (error message + success message)

--- src/components/ui/custom/error-message.tsx
+++ src/components/ui/custom/error-message.tsx
@@ -1,5 +1,19 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { AlertCircle } from "lucide-react"
+import { useTranslations } from "next-intl"
+
 export default function ErrorMessage({ message }: ErrorMessageProps) {
-  return <div className="text-red-500 text-center">{message}</div>
+  const t = useTranslations("global")
+
+  return (
+    <div className="flex w-full justify-center pt-10">
+      <Alert variant="destructive" className="w-fit">
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>{t("error")}</AlertTitle>
+        <AlertDescription>{message}</AlertDescription>
+      </Alert>
+    </div>
+  )
 }
 
 type ErrorMessageProps = {
--- src/components/ui/custom/success-message.tsx
+++ src/components/ui/custom/success-message.tsx
@@ -1,5 +1,19 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { useTranslations } from "next-intl"
+import { LuInfo } from "react-icons/lu"
+
 export default function SuccessMessage({ message }: SuccessMessageProps) {
-  return <div className="text-green-500">{message}</div>
+  const t = useTranslations("global")
+
+  return (
+    <div className="flex w-full justify-center pt-10">
+      <Alert className="w-fit text-verdant border-verdant">
+        <LuInfo className="h-4 w-4 text-verdant" color="#55ff99" />
+        <AlertTitle>{t("success")}</AlertTitle>
+        <AlertDescription>{message}</AlertDescription>
+      </Alert>
+    </div>
+  )
 }
 
 type SuccessMessageProps = {
