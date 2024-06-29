3b801cb9de0c79346dae11f07876f8f7ed24ebbc
Adam Mehdaoui <90783059+adammehdaoui@users.noreply.github.com>
2024-06-29 19:59:36 +0200

    [IN-124] Hotfix - English translation + update dislike after previous fix

--- src/lib/query/client/nfts/fetchDislikeNft.ts
+++ src/lib/query/client/nfts/fetchDislikeNft.ts
@@ -1,7 +1,9 @@
 import axiosClient from "@/lib/client"
 
-export default async function fetchDislikeNft(nftId: number) {
-  const { data } = await axiosClient.post<{ isLiked: boolean }>(`/nfts/${nftId.toString()}/dislike`)
+export default async function fetchDislikeNft(nftId: number, minterId: number) {
+  const { data } = await axiosClient.post<{ isLiked: boolean }>(
+    `/nfts/${nftId.toString()}/${minterId.toString()}/dislike`
+  )
   const { isLiked } = data
 
   return isLiked
